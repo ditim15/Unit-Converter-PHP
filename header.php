d0b445840721c50a2925fa6d94ea6a044ef25657
ditim15 <ditim456@gmail.com>
2026-08-07 11:33:08 -0400

    Added layout for header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unit Converter</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="header">
+        <h1>Unit Converter</h1>
+                <nav>
+            <a href="length.php">Length</a>
+            <a href="weight.php">Weight</a>
+            <a href="temperature.php">Temperature</a>
+        </nav>
+        <div id="conversion-area">
